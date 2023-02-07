288ebc364c2f6770e0570eceb726379b7c48ac8a
sree1034 <sreenath1034@gmail.com>
2023-02-07 11:38:18 +0530

    first code change

---
+++ js/main.js
@@ -0,0 +1,118 @@
+/*  ---------------------------------------------------
+    Template Name: Dreams
+    Description: Dreams wedding template
+    Author: Colorib
+    Author URI: https://colorlib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Portfolio filter
+        --------------------*/
+        $('.portfolio__filter li').on('click', function () {
+            $('.portfolio__filter li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.portfolio__gallery').length > 0) {
+            var containerEl = document.querySelector('.portfolio__gallery');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+		Hero Slider
+	--------------------*/
+    $('.hero__slider').owlCarousel({
+        loop: true,
+        dots: true,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        margin: 0,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+    });
+
+    var dot = $('.hero__slider .owl-dot');
+    dot.each(function () {
+        var index = $(this).index() + 1;
+        if (index < 10) {
+            $(this).html('0').append(index);
+        } else {
+            $(this).html(index);
+        }
+    });
+
+    /*------------------
+        Logo Slider
+    --------------------*/
+    $(".logo__carousel").owlCarousel({
+        loop: true,
+        margin: 100,
+        items: 6,
+        dots: false,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 5
+            },
+            768: {
+                items: 4
+            },
+            480: {
+                items: 3
+            },
+            320: {
+                items: 2
+            }
+        }
+    });
+
+    /*------------------
+        Counter
+    --------------------*/
+    $('.counter_num').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
